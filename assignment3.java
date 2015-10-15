fd432b6c5f6e00c05665e145cf4f22a19140c7b8
unknown <tsc2453@AN112500-CT103.nvstu.edu>
2015-10-15 16:56:45 -0400

    Logic Errors galore!

---
+++ assignment3.java
@@ -0,0 +1,63 @@
+package assignment3;
+
+import javax.swing.JOptionPane;
+
+public class a3 extends JOptionPane {
+	//This is a personal reminder not to mix while loops and for loops
+	//This is hard to follow.
+	public static void main(String[] args) {
+		String[] username = {"jacobf", "BobbySmith", "Mike"};
+		String[] password = {"HelloWorld", "IHateClass", "NeedCoffee"};
+		String[] accounttype = {"Admin", "Student", "Staff"};
+		int Try = 0;
+		int index = 0;
+		int trylimit = 3;
+		boolean correctuser = false;
+		boolean correctpw = false;
+		while (Try < trylimit){
+			if (correctuser == false){
+				String useruninput = JOptionPane.showInputDialog("Enter Username");
+				for (index=0; index<3; index++){
+					if (useruninput.equals(username[index])){
+						correctuser = true;
+
+						break;
+					}
+
+				}
+			}
+			else Try++;
+			if (correctuser == true && correctpw==false){
+				if(password[index].equals(JOptionPane.showInputDialog("Enter Password"))){
+					correctpw = true;
+					break;
+				}
+			}
+			else Try++;
+		}
+		while(correctuser == true && correctpw==true){
+
+			String[] choices = {"Admin" , "Student" , "Staff"};
+			String input = (String) JOptionPane.showInputDialog(null, "Choose account type:","Account Type",JOptionPane.QUESTION_MESSAGE, null, choices , choices[1]);
+			if (accounttype[index].equals(input)){	
+				correctuser=false;   			
+				switch(input){
+				case "Admin":
+					JOptionPane.showMessageDialog(null, "Welcome Admin! You can update and read files.");
+					break;
+				case "Student":
+					JOptionPane.showMessageDialog(null, "Welcome Student! You may read only read files");
+					break;
+				case "Staff":
+					JOptionPane.showMessageDialog(null, "Welcome Staff! You may add,delete,read and update files.");
+					break;
+
+				}
+
+			}
+		}
+		if (Try >= trylimit) {
+			JOptionPane.showMessageDialog(null,"You have reached the maximum log in attempts. Contact an administrator.");
+		}
+	}
+}	
